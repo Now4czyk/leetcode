22f1597f3fd98743f32a31c5f60e8db7e9d0bbdc
Kacper Nowaczyk <k.nowaczyk@teb-akademia.pl>
2024-01-09 10:08:36 +0100

    171-excel-sheet-column-number

---
+++ src/easy/171-excel-sheet-column-number.test.ts
@@ -0,0 +1,23 @@
+function titleToNumber(columnTitle: string): number {
+  let num = 0;
+  for (let i = 0; i < columnTitle.length; i++) {
+    num = num * 26 + (columnTitle[i].charCodeAt(0) - 64);
+  }
+  return num;
+}
+
+describe("171. Excel Sheet Column Number", () => {
+  it("Test 1", () => {
+    expect(titleToNumber("A")).toEqual(1);
+  });
+  it("Test 2", () => {
+    expect(titleToNumber("AB")).toEqual(28);
+  });
+  it("Test 3", () => {
+    expect(titleToNumber("ZY")).toEqual(701);
+  });
+
+  it("Test 4", () => {
+    expect(titleToNumber("FXSHRXW")).toEqual(2147483647);
+  });
+});
